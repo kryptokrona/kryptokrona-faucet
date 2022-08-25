7103fd8d734d4245fa41bc680c180e2183d60f36
Lukas <36674091+Swepool@users.noreply.github.com>
2022-08-25 14:32:07 +0200

    cors

--- main.js
+++ main.js
@@ -2,34 +2,36 @@ const express = require('express')();
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const rateLimit = require('express-rate-limit')
-const {initWallet} = require("./wallet");
+const { initWallet } = require("./wallet");
 
-const PORT = 8083
-const CORS_OPTIONS = {
-    origin: '*',
-    method: ['GET', 'POST']
-}
+const PORT = 8084
 
 const faucetLimiter = rateLimit({
     windowMs: 60 * 60 * 1000, // 60 minutes
-    max: 1, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    max: 1000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
 
+const corsOptions = {
+    origin: '*',
+    method: ['GET', 'POST']
+}
+
 //START WALLET
 initWallet().then(r => console.log('Wallet started'))
 
+express.use(cors());
 express.use(bodyParser.json())
 
 //Balance
 const statusRoute = require('./routes/status')
-express.use('/status', cors(CORS_OPTIONS), statusRoute)
+express.use('/status', cors(corsOptions), statusRoute)
 
 //Faucet
-express.use('/faucet', faucetLimiter)
 const faucetRoute = require('./routes/faucet')
-express.use('/faucet', cors(CORS_OPTIONS), faucetRoute)
+express.use(faucetRoute, faucetLimiter)
+express.use('/faucet', cors(corsOptions), faucetRoute)
 
 express.listen(
     PORT,
