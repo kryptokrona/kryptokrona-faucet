cecf275163c133f79a4daf5301a5438df164a30a
Lukas <36674091+Swepool@users.noreply.github.com>
2022-08-25 17:01:46 +0200

    add limit

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const PORT = 8084
 
 const faucetLimiter = rateLimit({
     windowMs: 60 * 60 * 1000, // 60 minutes
-    max: 1000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    max: 3, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
